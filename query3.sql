e68b75b110fa7d12d11b457ca9649891a4994039
GundaAruna <130962449+GundaAruna@users.noreply.github.com>
2023-04-28 22:30:48 +0530

    Create query3.sql

---
+++ query3.sql
@@ -0,0 +1,6 @@
+create database HeroVired;
+create table RGM(Name varchar(30),college varchar(30),session varchar(30),contact_no int,address varchar(30));
+INSERT INTO RGM VALUES ("Aruna","rgmcet","sql",1234,"Anantapur"),("Kavya","pvkk","python",1222,"Anantapur"),("Dhanusha","srit","java",1333,"kurnool");
+select *from RGM;
+INSERT INTO RGM VALUES ("Deekshitha","rgmcet","python",3456,"kadapa"),("Deepthi","pvkk","python",1222,"Anantapur"),("Dhanusha","srit","python",45689,"nellore");
+INSERT INTO RGM VALUES ("Vasantha","ssit","uit",1239,"kakinada");
