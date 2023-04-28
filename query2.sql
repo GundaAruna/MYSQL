7ec2cb6092beeca4e7cffdd325c6f2c1fd6ee369
GundaAruna <130962449+GundaAruna@users.noreply.github.com>
2023-04-28 22:30:09 +0530

    Create query2.sql

---
+++ query2.sql
@@ -0,0 +1,8 @@
+-- create database rgm;-- 
+select *from student;
+-- INSERT INTO student(student_id,student_name,student_contact,student_dept) VALUES (101,"aruna",12345665,"cse");
+INSERT INTO student VALUES (102,"dhanu",1234,"ece");
+INSERT INTO student VALUES (103,"ananya",3345,"civil");
+INSERT INTO student VALUES (104,"abhi",1819,"eee");
+INSERT INTO student VALUES (105,"suguna",1896,"mech");
+create database HeroVired;
